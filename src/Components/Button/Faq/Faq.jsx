5fab1af5778e97bddc761975af6ea0addf084f6e
MoRsHeD <mdmorshed0187@gmail.com>
2024-01-17 01:50:08 +0600

    testimonial section added

--- src/Components/Button/Faq/Faq.jsx
+++ src/Components/Button/Faq/Faq.jsx
@@ -5,54 +5,57 @@ const Faq = () => {
     const [datas, setDatas] = useState();
 
     // add your array of object data 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('faq.json')
-        .then(res => res.json())
-        .then(data => setDatas(data.faqs))
-    },[])
+            .then(res => res.json())
+            .then(data => setDatas(data.faqs))
+    }, [])
 
     // toggle state and function 
     const [isOpen, setIsOpen] = useState(null);
     const handleToggle = (idx) => setIsOpen((prevIdx) => (prevIdx === idx ? null : idx));
 
     return (
-        <div className="flex justify-between items-center gap-5 max-w-6xl mx-auto my-14">
-            {/* faq images */}
-            <div className="flex-1">
-                <img src="https://i.ibb.co/VvgxsyS/Questions-pana.png" alt="" />
-            </div>
-            {/* questions */}
-            <div className="rounded-lg py-20 space-y-6 cursor-pointer">
-                {/* maping each accordion  */}
-                {datas?.map((data, idx) => (
-                    <div key={idx} onClick={() => handleToggle(idx)} className="flex items-center">
-                        {/* the index div  */}
-                        <div className="w-16 h-16 bg-[#409bd4] flex justify-center items-center text-white text-2xl font-semibold rounded-xl font-sans">
-                            <span>0{idx + 1}</span>
-                        </div>
-                        <div className="w-10 h-[2px] bg-[#409bd4] relative">
-                            <span className="w-3 h-3 bg-white absolute -left-2 -top-[5px] z-40 rounded-full border-2 border-[#409bd4]"></span>
-                            <span className="bg-[#409bd4] w-10 h-1"></span>
-                        </div>
-                        {/* main accordion div  */}
-                        <div>
-                            <div className="max-w-[450px] bg-sky-50 shadow-md border-t-[12px] p-3 border-[#409bd4] relative">
-                                <span className="h-0 w-0 border-b-[40px] border-b-transparent border-r-[40px] border-r-[#409bd4] absolute top-0 right-0"></span>
-                                <h1 className="text-[#409bd4] text-xl text-center">{data?.question}</h1>
+        <div className="max-w-6xl mx-auto my-14">
+            <h2 className="text-5xl font-bold text-center mb-5">Frequently Asked Questions</h2>
+            <div className="flex justify-between items-center gap-5">
+                {/* faq images */}
+                <div className="flex-1">
+                    <img src="https://i.ibb.co/VvgxsyS/Questions-pana.png" alt="" />
+                </div>
+                {/* questions */}
+                <div className="rounded-lg py-20 space-y-6 cursor-pointer">
+                    {/* maping each accordion  */}
+                    {datas?.map((data, idx) => (
+                        <div key={idx} onClick={() => handleToggle(idx)} className="flex items-center">
+                            {/* the index div  */}
+                            <div className="w-16 h-16 bg-[#409bd4] flex justify-center items-center text-white text-2xl font-semibold rounded-xl font-sans">
+                                <span>0{idx + 1}</span>
                             </div>
-                            <div
-                                className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600  ${isOpen === idx ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'
-                                    }`}
-                            >
-                                <div className="overflow-hidden">
-                                    <div className=" max-w-[450px] rounded-br-xl rounded-bl-xl bg-[#409bd4] text-white p-6 text-center text-sm">
-                                        {data?.answer}
+                            <div className="w-10 h-[2px] bg-[#409bd4] relative">
+                                <span className="w-3 h-3 bg-white absolute -left-2 -top-[5px] z-40 rounded-full border-2 border-[#409bd4]"></span>
+                                <span className="bg-[#409bd4] w-10 h-1"></span>
+                            </div>
+                            {/* main accordion div  */}
+                            <div>
+                                <div className="max-w-[450px] bg-sky-50 shadow-md border-t-[12px] p-3 border-[#409bd4] relative">
+                                    <span className="h-0 w-0 border-b-[40px] border-b-transparent border-r-[40px] border-r-[#409bd4] absolute top-0 right-0"></span>
+                                    <h1 className="text-[#409bd4] text-xl text-center">{data?.question}</h1>
+                                </div>
+                                <div
+                                    className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600  ${isOpen === idx ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'
+                                        }`}
+                                >
+                                    <div className="overflow-hidden">
+                                        <div className=" max-w-[450px] rounded-br-xl rounded-bl-xl bg-[#409bd4] text-white p-6 text-center text-sm">
+                                            {data?.answer}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,9 +1,84 @@
-
+import Rating from 'react-rating';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Autoplay, FreeMode, Pagination } from 'swiper/modules';
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { FaQuoteRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useEffect, useState } from 'react';
 
 const Testimonials = () => {
+    const [reviewData, setReviewData] = useState();
+
+    useEffect(()=>{
+        fetch('reviewData.json')
+        .then(res => res.json())
+        .then(data => setReviewData(data?.reviews))
+    },[])
+
+    // console.log(reviewData)
+
     return (
-        <div>
-            
+        <div className='max-w-6xl mx-auto my-14'>
+            <h2 className="text-5xl font-bold text-center mb-5">Testimonials</h2>
+            <Swiper
+                breakpoints={{
+                    425: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    768: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                    1024: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    }
+                }}
+                freeMode={true}
+                pagination={{
+                    clickable: true,
+                }}
+                autoplay={{ delay: 1500, disableOnInteraction: false }}
+                modules={[FreeMode, Pagination, Autoplay]}
+                className="mySwiper"
+            >
+                {
+                    reviewData?.map(review => <SwiperSlide className="px-6 py-10" key={review.name}>
+                        <div className="border-2 p-4 rounded-xl hover:border-black flex">
+                            <div className="px-4 py-8 bg-base-200 rounded-xl text-start">
+                                <Rating
+                                    initialRating={review.rating}
+                                    emptySymbol={<AiOutlineStar className="text-orange-300 w-8 h-8" />}
+                                    fullSymbol={<AiFillStar className="text-orange-300 w-8 h-8" />}
+                                ></Rating>
+                                <p className="text-lg text-[#222e48] my-6 text-start min-h-[100px]">
+                                    {review.details}
+                                </p>
+                                <div className="flex flex-col-reverse md:flex-row items-center justify-between">
+                                    <div className="flex flex-col md:flex-row items-center gap-3">
+                                        <div className="w-14 h-14">
+                                            <img src={review.img} alt="" className="rounded-full " />
+                                        </div>
+                                        <div className="text-start text-[#222e48]">
+                                            <h4 className="text-xl font-semibold mb-2">{review.name}</h4>
+                                            <h6 className="font-medium">{review.email}</h6>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <FaQuoteRight className="w-14 h-14 text-base-300" />
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </SwiperSlide>)
+                }
+                <div className="mb-10"></div>
+
+            </Swiper>
         </div>
     );
 };
