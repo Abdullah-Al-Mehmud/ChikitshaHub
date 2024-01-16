4e825a33297f5dd119374f6a6c472fc02f6cc9ce
mehmud <timehmud121@gmail.com>
2024-01-17 02:10:39 +0600

    added some updates to hero section

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
   const links = (
     <>
       <li>
-        <a>Item 1</a>
+        <a>Home</a>
       </li>
       <li>
-        <a>Parent</a>
+        <a>About Us</a>
       </li>
       <li>
-        <a>Item 3</a>
+        <a>Contact Us</a>
       </li>
     </>
   );
@@ -58,7 +58,30 @@ const Header = () => {
           <ul className="menu menu-horizontal px-1">{links}</ul>
         </div>
 
-        <a className="btn">Button</a>
+        <button className="flex items-center relative w-32 border-2 border-[#409bd4] text-[#409bd4] p-2 rounded-full group">
+          <span>Get Start</span>
+          <span className="absolute w-1/6 right-3 group-hover:w-5/6 box-content duration-300 flex justify-center bg-white rounded-full">
+            <svg
+              viewBox="0 0 25 25"
+              fill="none"
+              className=""
+              xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  d="M4 12H20M20 12L14 6M20 12L14 18"
+                  stroke="#409bd4"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"></path>
+              </g>
+            </svg>
+          </span>
+        </button>
       </div>
     </div>
   );
