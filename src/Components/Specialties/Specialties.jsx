8e4846b4f78d6e068321b7cf44f9faa7806bda49
Rashed <rashedulhoqueceo@gmail.com>
2024-01-17 00:36:20 +0600

    completed specialties component

--- src/Components/Specialties/Specialties.jsx
+++ src/Components/Specialties/Specialties.jsx
@@ -4,6 +4,7 @@ import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 import { Autoplay, FreeMode, Pagination } from 'swiper/modules';
 import { useEffect, useState } from 'react';
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Specialties = () => {
 
@@ -18,7 +19,8 @@ const Specialties = () => {
 
 
     return (
-        <div className='max-w-6xl mx-auto'>
+        <div className='max-w-6xl mx-auto my-14'>
+            <h2 className='text-2xl md:text-3xl lg:text-4xl font-bold text-black text-center mb-6'>Specialties</h2>
             <Swiper
                 breakpoints={{
                     425: {
@@ -30,7 +32,7 @@ const Specialties = () => {
                         spaceBetween: 20,
                     },
                     1024: {
-                        slidesPerView: 3,
+                        slidesPerView: 4,
                         spaceBetween: 20,
                     }
                 }}
@@ -44,15 +46,21 @@ const Specialties = () => {
             >
                 {
                     specialtiesData.slice(0, 6).map(category => <SwiperSlide className="px-6 py-10" key={category.id}>
-                        <div className="border-2 p-4 rounded-xl">
-                            <img src={category.img} alt="" />
-                            <h5 className='text-xl font-medium text-center my-4'>{category.name}</h5>
+                        <div className="border-2 px-4 py-8 rounded-xl hover:border-[#409bd4]">
+                            <div className='bg-base-300 rounded-full w-fit mx-auto p-4'>
+                                <img className='w-28 h-28 mx-auto p-4' src={category.img} alt="" />
+                            </div>
+                            <h5 className='text-xl font-medium text-center my-6 min-h-14'>{category.name}</h5>
                         </div>
                     </SwiperSlide>)
                 }
                 <div className="mb-10"></div>
 
             </Swiper>
+
+            <button className="flex items-center relative w-28 mx-auto border-2 border-[#409bd4] text-[#409bd4] p-4 rounded-lg group mt-4"><span>See All</span><span className="absolute w-1/6 right-3 group-hover:w-5/6 box-content duration-300 flex justify-center bg-white rounded-lg">
+                <FaArrowRightLong/>
+                </span></button>
         </div>
     );
 };
