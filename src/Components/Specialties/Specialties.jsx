261b77e7174b000f86b29aa1acc8b78d29d103d4
Rashed <rashedulhoqueceo@gmail.com>
2024-01-17 00:06:50 +0600

    create specialties json data

--- src/Components/Specialties/Specialties.jsx
+++ src/Components/Specialties/Specialties.jsx
@@ -18,7 +18,7 @@ const Specialties = () => {
 
 
     return (
-        <div>
+        <div className='max-w-6xl mx-auto'>
             <Swiper
                 breakpoints={{
                     425: {
@@ -43,20 +43,10 @@ const Specialties = () => {
                 className="mySwiper"
             >
                 {
-                    specialtiesData.slice(0, 6).map(loan => <SwiperSlide className="px-6 py-10" key={loan._id}>
+                    specialtiesData.slice(0, 6).map(category => <SwiperSlide className="px-6 py-10" key={category.id}>
                         <div className="border-2 p-4 rounded-xl">
-                            <div className="px-4 py-8 bg-base-200 text-[#222e48] rounded-xl hover:text-white hover:bg-[#074c3e]">
-                                <div className="h-20 w-20 mx-auto bg-white p-3 rounded-full mb-6">
-                                    <img className="w-6 h-6" src={loan.icons} alt="" />
-                                </div>
-                                <h2 className="text-2xl font-bold hover:text-[#fcb650] cursor-pointer">{loan.title}</h2>
-                                <p className="my-6 min-h-[96px]">{loan.description.length > 100 ? loan.description.slice(0, 100) + '...' : loan.description}</p>
-                                {/* <Link to={`/service/details/${loan._id}`}>
-                                    <button className="rounded-full bg-white p-3 text-[#074c3e] hover:bg-[#fcb650] hover:text-white">
-                                        <MdNavigateNext className="w-6 h-6"/>
-                                    </button>
-                                </Link> */}
-                            </div>
+                            <img src={category.img} alt="" />
+                            <h5 className='text-xl font-medium text-center my-4'>{category.name}</h5>
                         </div>
                     </SwiperSlide>)
                 }
