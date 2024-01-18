af366f209788778d910beb66601426897702b08a
TahidulAlam <tahid.cse@gmail.com>
2024-01-19 01:03:30 +0600

    upd

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,11 @@
 import Header from "../../Shared/Header/Header";
+import Banner from "./Banner/Banner";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  );
 };
-
-export default Home;
