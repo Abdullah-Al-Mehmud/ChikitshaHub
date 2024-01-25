effe631d4c48bb75a10371a3e008587a57f2e6af
morsalinamunmun <morsalinamunmun@gmail.com>
2024-01-25 11:07:49 +0600

    BMI status check & set

--- src/Components/BMICalculator/BMICalculator.jsx
+++ src/Components/BMICalculator/BMICalculator.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
 
 const BMICalculator = () => {
+    const [height, setHeight] = useState(null);
+    const [weight, setWeight] = useState(null);
+    const [bmiResult, setBmiResult] = useState(null);
+    const [status, setStatus] = useState('');
+
+    function calculateBMI(){
+        let bmi = Number(weight / (height / 100) ** 2).toFixed(2);
+        setBmiResult(bmi);
+        let bmiStatus = getStatus(bmi)
+        setStatus(bmiStatus)
+    }
+
+    function getStatus(bmi){
+        if(bmi < 18.5){
+            return "underweight";
+        } else if(bmi >= 18.5 && bmi < 24.9){
+            return "Normal";
+        } else if(bmi >= 25 && bmi < 29.9){
+            return "Overweight"
+        }else return "Obese"
+    }
     return (
         <div className="min-h-screen bg-base-200">
             <h1 className="text-3xl py-10 text-center font-bold">BMI Calculation</h1>
@@ -12,16 +34,17 @@ const BMICalculator = () => {
                                 <label className="label">
                                     <span className="label-text">Weight</span>
                                 </label>
-                                <input type="number" placeholder="Weight" className="input input-bordered" required />
+                                <input type="weight in kg" id="weight" value={weight} onChange={e => setWeight(e.target.value)} placeholder="Weight" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Height</span>
                                 </label>
-                                <input type="number" placeholder="Height" className="input input-bordered" required />
+                                <input type="text" id="height in cm" value={height} onChange={e => setHeight(e.target.value)} placeholder="Height" className="input input-bordered" required />
                             </div>
+                            <p>{status} {bmiResult}</p>
                             <div className="form-control mt-6">
-                                <button className="btn bg-[#409bd4] text-white">BMI Calculate</button>
+                                <button onClick={calculateBMI} className="btn bg-[#409bd4] text-white">BMI Calculate</button>
                             </div>
                         </form>
                     </div>
