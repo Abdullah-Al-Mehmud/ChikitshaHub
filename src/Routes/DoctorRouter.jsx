bd0fff26b01b94362dfd692c08717727895c8fef
Rashed <rashedulhoqueceo@gmail.com>
2024-02-14 15:48:14 +0600

    create doctor router

--- src/Routes/DoctorRouter.jsx
+++ src/Routes/DoctorRouter.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types';
-import useAdmin from '../Hooks/useAdmin';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import useDoctor from '../Hooks/useDoctor';
 
 
-const AdminRouter = ({ children }) => {
+const DoctorRouter = ({ children }) => {
 
     const user = useSelector((state) => state.auth.user);
     const isLoading = useSelector((state) => state.auth.loading);
-    const [isAdmin, isAdminLoading] = useAdmin();
+    const [isDoctor, isDoctorLoading] = useDoctor();
     const location = useLocation();
 
-    if (isLoading || isAdminLoading) {
+    if (isLoading || isDoctorLoading) {
         return (
             <div className="flex justify-center items-center h-screen">
                 <span className="loading loading-dots loading-lg "></span>
@@ -19,14 +19,14 @@ const AdminRouter = ({ children }) => {
         )
     }
 
-    if (user && isAdmin) {
+    if (user && isDoctor) {
         return children
     }
     return <Navigate to="/" state={{ from: location }} replace></Navigate>
 };
 
-AdminRouter.propTypes = {
+DoctorRouter.propTypes = {
     children: PropTypes.node,
 }
 
-export default AdminRouter;
+export default DoctorRouter;
