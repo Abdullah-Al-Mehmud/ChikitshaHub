6816979667dc614680ef031e61f6119680f8dffe
Rashed <rashedulhoqueceo@gmail.com>
2024-02-14 15:45:59 +0600

    create admin router

---
+++ src/Routes/AdminRouter.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import useAdmin from '../Hooks/useAdmin';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+
+const AdminRouter = ({children}) => {
+    
+    const user = useSelector((state) => state.auth.user);
+    const isLoading = useSelector((state) => state.auth.loading);
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+
+    if(isLoading || isAdminLoading){
+        return <p>loading</p>
+    }
+
+    if(user && isAdmin){
+        return children
+    }
+    return <Navigate to="/" state={{from: location}} replace></Navigate>
+};
+
+AdminRouter.propTypes = {
+    children: PropTypes.node,
+}
+
+export default AdminRouter;
