be52c0e85156b8a4f89fcf47cb32189ff3964ef9
Rashed <rashedulhoqueceo@gmail.com>
2024-02-25 13:07:26 +0600

    change some ui from meet page

--- src/Pages/Meet/Meet.jsx
+++ src/Pages/Meet/Meet.jsx
@@ -1,54 +1,50 @@
 import { useParams } from "react-router-dom";
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
-
 const Meet = () => {
+  const [currentUser, setCurrentUser] = useState("");
 
-    const [currentUser, setCurrentUser] = useState('');
+  const { meetId } = useParams();
+  const user = useSelector((state) => state.auth.user);
+  const axios = useAxiosPublic();
+  useEffect(() => {
+    axios.get(`/users/${user?.email}`).then((res) => setCurrentUser(res.data));
+  }, [axios, user?.email]);
 
-    const { meetId } = useParams();
-    const user = useSelector((state) => state.auth.user);
-    const axios = useAxiosPublic();
-    useEffect(() => {
-        axios.get(`/users/${user?.email}`)
-            .then(res => setCurrentUser(res.data))
-    }, [axios, user?.email])
+  const userId = currentUser[0]?._id;
+  const userName = currentUser[0]?.name;
 
+  // console.log(userId, userName)
 
-    const userId = currentUser[0]?._id;
-    const userName = currentUser[0]?.name;
-
-    // console.log(userId, userName)
-
-    const myMeeting = async (element) => {
-        const appID = parseInt(import.meta.env.VITE_APP_ID);
-        const serverSecret = import.meta.env.VITE_SERVER_SECRET;
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-            appID,
-            serverSecret,
-            meetId,
-            userId,
-            userName);
-        const zc = ZegoUIKitPrebuilt.create(kitToken);
-
-        zc.joinRoom({
-            container: element,
-            scenario: {
-                mode: ZegoUIKitPrebuilt.OneONoneCall,
-            }
-        });
-    }
-    // console.log(currentUser[0]._id)
-
-    return (
-        <div className="max-w-6xl mx-auto px-6 mt-16 lg:py-20">
-            <div ref={myMeeting}></div>
-            
-        </div>
+  const myMeeting = async (element) => {
+    const appID = parseInt(import.meta.env.VITE_APP_ID);
+    const serverSecret = import.meta.env.VITE_SERVER_SECRET;
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      meetId,
+      userId,
+      userName
     );
+    const zc = ZegoUIKitPrebuilt.create(kitToken);
+
+    zc.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      },
+    });
+  };
+  // console.log(currentUser[0]._id)
+
+  return (
+    <div className="max-w-6xl mx-auto px-6 mt-16 lg:py-20">
+      <div ref={myMeeting}></div>
+    </div>
+  );
 };
 
 export default Meet;
